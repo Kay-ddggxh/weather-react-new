f02e3e36e10626e8deaf9c289a092a3fd3e04ebc
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2021-01-28 11:42:34 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        <a
-          href="https://github.com/Kathrin-ddggxh/weather-react-new"
-          target="_blank"
-        >
-          Open source code{" "}
-        </a>
-        by Kathrin Welfare
-      </footer>
+      <div className="container">
+        <footer>
+          <a
+            href="https://github.com/Kathrin-ddggxh/weather-react-new"
+            target="_blank"
+          >
+            Open source code{" "}
+          </a>
+          by Kathrin Welfare
+        </footer>
+      </div>
     </div>
   );
 }
