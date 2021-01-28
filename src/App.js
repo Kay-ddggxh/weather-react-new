f1a13c18f91e39f7caafe68824571ffd62e92805
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2021-01-28 12:03:35 +0000

    Created Weather component with basic html structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <footer>
           <a
             href="https://github.com/Kathrin-ddggxh/weather-react-new"
